cfb4067050c9226b605fac1d741655443ed90ea1
Mateus Miranda <info.mateusmiranda@gmail.com>
2017-06-08 10:37:58 -0300

    colocando webpack e mudando para provider

--- src/web-storage.js
+++ src/web-storage.js
@@ -1,17 +1,22 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 (function () {
   'use strict';
 
   WebStorage.$inject = [];
   function WebStorage() {
     return {
-      setSessionStorageItem: function (key, value) {
+      setSessionStorageItem: function setSessionStorageItem(key, value) {
         var _value = value;
-        if (typeof value == 'object') {
+        if ((typeof value === 'undefined' ? 'undefined' : _typeof(value)) == 'object') {
           _value = angular.toJson(value);
         }
         window.sessionStorage.setItem(key, _value);
       },
-      getSessionStorageItem: function (key) {
+      getSessionStorageItem: function getSessionStorageItem(key) {
         var g = window.sessionStorage.getItem(key);
         if (!g) {
           return null;
@@ -23,19 +28,19 @@
         }
         return angular.fromJson(g);
       },
-      removeSessionStorageItem: function (key) {
+      removeSessionStorageItem: function removeSessionStorageItem(key) {
         window.sessionStorage.removeItem(key);
       },
-      clearSessionStorage: function () {
+      clearSessionStorage: function clearSessionStorage() {
         window.sessionStorage.clear();
       },
-      getNumberOfItemsInSessionStorage: function () {
+      getNumberOfItemsInSessionStorage: function getNumberOfItemsInSessionStorage() {
         return window.sessionStorage.length;
       },
-      setLocalStorageItem: function (key, value) {
+      setLocalStorageItem: function setLocalStorageItem(key, value) {
         window.localStorage.setItem(key, angular.toJson(value));
       },
-      getLocalStorageItem: function (key) {
+      getLocalStorageItem: function getLocalStorageItem(key) {
         var g = window.localStorage.getItem(key);
         try {
           angular.fromJson(g);
@@ -44,18 +49,30 @@
         }
         return angular.fromJson(g);
       },
-      removeLocalStorageItem: function (key) {
+      removeLocalStorageItem: function removeLocalStorageItem(key) {
         window.localStorage.removeItem(key);
       },
-      clearLocalStorage: function () {
+      clearLocalStorage: function clearLocalStorage() {
         window.localStorage.clear();
       },
-      getNumberOfItemsInLocalStorage: function () {
+      getNumberOfItemsInLocalStorage: function getNumberOfItemsInLocalStorage() {
         return window.localStorage.length;
       }
-    }
+    };
+
+  }
+
+  if(!window.angular){
+    throw 'Gumga Web Storage requires the AngularJS library. See -> https://angularjs.org';
   }
 
   angular.module('gumga.webstorage', [])
-    .factory('GumgaWebStorage', WebStorage)
+         .provider('GumgaWebStorage', function(){
+           this.$get = function () {
+              return WebStorage();
+            }
+         });
+
 })();
+
+},{}]},{},[1]);
